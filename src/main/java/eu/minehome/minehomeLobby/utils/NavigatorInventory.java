41fd2a1b74a1b1b90c07157dbd09926d4d045ffc
MineHomeDev <klauthjens@web.de>
2025-03-05 13:06:03 +0100

    Increase GUI rows and update item actions in Navigator
    Expanded the inventory GUI from four rows to five, adjusted item positions, and renamed "JumpandRum" to "Coming Soon". Added placeholders for future features with appropriate messages for unavailable actions. Improved text formatting for better consistency.

--- src/main/java/eu/minehome/minehomeLobby/utils/NavigatorInventory.java
+++ src/main/java/eu/minehome/minehomeLobby/utils/NavigatorInventory.java
@@ -6,9 +6,13 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import javax.swing.*;
+
+import static eu.minehome.minehomeLobby.data.Data.prefix;
+
 public class NavigatorInventory extends SimpleMenu {
     public NavigatorInventory() {
-        super(Rows.FOUR, Component.text("§b§lNavigator"));
+        super(Rows.FIVE, Component.text("§b§lNavigator"));
 
     }
     @Override
@@ -16,23 +20,55 @@ public class NavigatorInventory extends SimpleMenu {
         // This method can be used to add items to the GUI
         final ItemStack Spawn = new ItemStack(Material.MAGMA_CREAM);
         final ItemMeta spawn = Spawn.getItemMeta();
-        spawn.displayName(Component.text("Spawn"));
+        spawn.displayName(Component.text("§b§lSpawn"));
         Spawn.setItemMeta(spawn);
 
-        final ItemStack JumpandRum = new ItemStack(Material.BARRIER);
-        final ItemMeta jumpandrun= JumpandRum.getItemMeta();
-        jumpandrun.displayName(Component.text("JumpandRum"));
-        JumpandRum.setItemMeta(jumpandrun);
+        final ItemStack ComingSoon = new ItemStack(Material.BARRIER);
+        final ItemMeta comingsoon= ComingSoon.getItemMeta();
+        comingsoon.displayName(Component.text("§b§lComing Soon"));
+        ComingSoon.setItemMeta(comingsoon);
 
-        setItem(31, Spawn, player -> {
+        setItem(22, Spawn, player -> {
             // This will be executed when the player clicks this item
             player.performCommand("spawn");
             player.closeInventory();
         });
 
-        setItem(22, JumpandRum, player -> {
-            player.performCommand("warp jumpandrun");
+        setItem(11, ComingSoon, player -> {
+            // This will be executed when the player clicks this item
+            player.sendMessage(prefix + "§c§lComing Soon");
             player.closeInventory();
         });
+
+        setItem(15, ComingSoon, player -> {
+            // This will be executed when the player clicks this item
+            player.sendMessage(prefix + "§c§lComing Soon");
+            player.closeInventory();
+        });
+
+        setItem(19, ComingSoon, player -> {
+            // This will be executed when the player clicks this item
+            player.sendMessage(prefix + "§c§lComing Soon");
+            player.closeInventory();
+        });
+
+        setItem(25, ComingSoon, player -> {
+            // This will be executed when the player clicks this item
+            player.sendMessage(prefix + "§c§lComing Soon");
+            player.closeInventory();
+        });
+
+        setItem(29, ComingSoon, player -> {
+            // This will be executed when the player clicks this item
+            player.sendMessage(prefix + "§c§lComing Soon");
+            player.closeInventory();
+        });
+        setItem(33, ComingSoon, player -> {
+            // This will be executed when the player clicks this item
+            player.sendMessage(prefix + "§c§lComing Soon");
+            player.closeInventory();
+        });
+
+
     }
 }
